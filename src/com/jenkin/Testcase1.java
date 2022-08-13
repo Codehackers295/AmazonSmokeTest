e6764761751d1c3155762678b7010ad7dfaac27f
Codehackers295 <okunal949@gmail.com>
2022-08-13 13:10:02 +0530

    commit the changes

--- src/com/jenkin/Testcase1.java
+++ src/com/jenkin/Testcase1.java
@@ -3,7 +3,7 @@ package com.jenkin;
 public class Testcase1 {
 
 	public static void main(String[] args) {
-		System.out.println("My first jenkin code push to git");
+		System.out.println("My first jenkin code push");
 
 	}
 
